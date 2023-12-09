2ad522bf2b45d8ffd3615f5b6ec9990105fba9d1
Thomas PIERRE <tpierre@atecna.fr>
2023-12-09 10:49:23 +0100

    install firebase

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		alias: {
 			src: './src',
 			utils: './src/utils',
 			components: './src/components',
 			styles: './src/styles',
-			lib: './lib',
 			routes: './src/routes',
 			stores: './src/stores',
 			modules: './node_modules'
