bfc73a23b15c7e4ae99667ad664be7571eed986c
Thomas PIERRE <tpierre@atecna.fr>
2023-12-09 10:54:24 +0100

    setup firebase

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,14 @@
 export const prerender = true;
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBVTnXwQleHEO_lRTVbDcCiddncghUFvyY',
+	authDomain: 'synth-cd39e.firebaseapp.com',
+	projectId: 'synth-cd39e',
+	storageBucket: 'synth-cd39e.appspot.com',
+	messagingSenderId: '988064059644',
+	appId: '1:988064059644:web:01939d30e74bd558e08779'
+};
+
+const app = initializeApp(firebaseConfig);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,3 @@
-<script context="module">
-	import { initializeApp } from 'firebase/app';
-
-	const firebaseConfig = {
-		apiKey: 'AIzaSyBVTnXwQleHEO_lRTVbDcCiddncghUFvyY',
-		authDomain: 'synth-cd39e.firebaseapp.com',
-		projectId: 'synth-cd39e',
-		storageBucket: 'synth-cd39e.appspot.com',
-		messagingSenderId: '988064059644',
-		appId: '1:988064059644:web:01939d30e74bd558e08779'
-	};
-
-	const app = initializeApp(firebaseConfig);
-</script>
-
 <script>
 	import 'styles/base.scss';
 	import Logo from 'components/Logo/Logo.svelte';
