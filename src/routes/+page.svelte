26a6401db7a9c32ebaed509c4406d505974e5aff
Thomas PIERRE <tpierre@atecna.fr>
2023-12-30 13:51:06 +0100

    add select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
 	import Controls from 'components/Controls/Controls.svelte';
 	import Panel from 'components/Panel/Panel.svelte';
 	import Keyboard from 'components/Keyboard/Keyboard.svelte';
+
+	let octaves = 3;
+	let from = 3;
 </script>
 
 <main>
@@ -12,10 +15,29 @@
 	<section id="logo"><Logo /></section>
 	<section id="top"><Panel /></section>
 	<section id="left"><Controls /></section>
-	<section id="keyboard"><Keyboard octaves={3} from={3} /></section>
+	<section id="keyboard"><Keyboard {octaves} {from} /></section>
 	<section id="right">&nbsp;</section>
 
 	<div class="border-right" />
+
+	<code>
+		<strong>
+			<label for="octaves">Octaves</label>
+			<select name="octaves" id="octaves" bind:value={octaves}>
+				<option value="3">3</option>
+				<option value="4">4</option>
+			</select>
+		</strong>
+
+		<strong>
+			<label for="from">From</label>
+			<select name="from" id="from" bind:value={from}>
+				{#each new Array(5) as _, i}
+					<option value={i}>{i}</option>
+				{/each}
+			</select>
+		</strong>
+	</code>
 </main>
 
 <style lang="scss">
